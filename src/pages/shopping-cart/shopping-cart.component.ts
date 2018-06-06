12e9ffad84ce7fdbc43003c24150c637fa8a0f1b
PoojaKalange <pujacompu@gmail.com>
2018-06-06 11:46:54 +0530

    Chnages
    Updated Cart pages

--- src/pages/shopping-cart/shopping-cart.component.ts
+++ src/pages/shopping-cart/shopping-cart.component.ts
@@ -27,20 +27,13 @@ export class ShoppingCartComponent implements OnInit, OnDestroy {
   private cartSubscription: Subscription;
 
   public constructor(private productsService: GetDataProvider,
-                     private shoppingCartService: ShoppingCartService,private nav: NavController ) {
+    private shoppingCartService: ShoppingCartService, private nav:NavController ) {
   }
 
 public OpenMenuPage():void
 {
   this.nav.push(HomePage);
 }
-
-public deleteItemfrmCart()
-{
-  console.log("hahahhaha");
-  
-}
-
 
   public emptyCart(): void {
     this.shoppingCartService.empty();
@@ -51,7 +44,6 @@ public deleteItemfrmCart()
     this.cart = this.shoppingCartService.get();
     console.log(this.cart);
     this.cartSubscription = this.cart.subscribe((cart) => {
-
       this.itemCount = cart.items.map((x) => x.Quantity).reduce((p, n) => p + n, 0);
       cart.items.forEach(element => {
 
@@ -71,14 +63,14 @@ public deleteItemfrmCart()
             }
           }
         }
-      cart.items.forEach(element => {
 
-        this.cartArray.push(element);
-        
+
       });
 
-      console.log("Product details: ",this.cartArray);
+      console.log("Product details: ", this.cartArray);
     });
+
+    console.log(this.cartSubscription);
   }
 
 //add item to cart
@@ -110,6 +102,9 @@ public deleteItemfrmCart()
       sub.unsubscribe();
     });
   }
+
+
+
   public ngOnDestroy(): void {
     if (this.cartSubscription) {
       this.cartSubscription.unsubscribe();
